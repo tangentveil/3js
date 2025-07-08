c4aed5efcf4986beeff50aea25fa5e149f4f1e16
tangentveil <anirudha0789@gmail.com>
2025-07-08 12:11:50 +0530

    react, react-three-fiber, drei

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,16 +1,79 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const canvas = document.getElementById("canvas");
 
-// 1. scene
-
+// 1. Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("#f0f0f0");
 
-// 2.camera
+// 2. Camera
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+
+camera.position.z = 5;
+
+// 3. Object
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshLambertMaterial({
+  color: "#468585",
+  emissive: "#468585",
+});
+
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  color: "#b4b4b3",
+  emissive: "#b4b4b3",
+});
+
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+
+// 4. Light
+const light = new THREE.SpotLight(0x006769, 100);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+// 5. Rendrer
+const rendrer = new THREE.WebGLRenderer({ canvas });
+rendrer.setSize(window.innerWidth, window.innerHeight);
+rendrer.setPixelRatio(window.devicePixelRatio);
+
+// 6. Add OrbitControls
+const controls = new OrbitControls(camera, rendrer.domElement);
+controls.enableDamping = true;
+controls.dampinFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePen = true;
+
+// 7. Add Animation
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  dodecahedron.rotation.x += 0.01;
+  dodecahedron.rotation.y += 0.01;
+
+  box.rotation.y += 0.005;
+
+  controls.update();
+  rendrer.render(scene, camera);
+}
+
+// 8. Handle window resizing
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  rendrer.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
