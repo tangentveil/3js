4ef6360b9d5f152f8c8ebc4e54a3aa4848ff7f12
tangentveil <anirudha0789@gmail.com>
2025-07-26 11:30:27 +0530

    .

--- 3d-Ai-website/server/routes/dalle.routes.js
+++ 3d-Ai-website/server/routes/dalle.routes.js
@@ -1,6 +1,6 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import OpenAI from "openai";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import OpenAI from 'openai';
 
 dotenv.config();
 
@@ -10,30 +10,24 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-router.route("/").get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
-});
-
-router.route("/").post(async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const { prompt } = req.body;
 
     const response = await openai.images.generate({
-      model: "dall-e-3", // or "dall-e-2" depending on access
-      prompt: prompt,
+      prompt,
       n: 1,
-      size: "1024x1024",
-      response_format: "b64_json",
+      size: '1024x1024',
+      response_format: 'b64_json',
     });
 
-    console.log(response)
+    console.log(response);
+    const image = response.data[0].b64_json;
 
-    const image_base64 = response.data[0].b64_json;
-
-    res.status(200).json({ photo: image_base64 });
+    res.status(200).json({ photo: image });
   } catch (error) {
-    console.error("OpenAI Error:", error);
-    res.status(500).json({ message: "Something went wrong" });
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong' });
   }
 });
 
